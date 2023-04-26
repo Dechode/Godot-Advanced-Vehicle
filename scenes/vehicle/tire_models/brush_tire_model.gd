6184318a86d1cd4a307d319635fe625f116fee57
Veeti Väisänen <veetiv.aisanen1@gmail.com>
2023-04-26 22:36:09 +0300

    Fix for brush model on low slips

--- scenes/vehicle/tire_models/brush_tire_model.gd
+++ scenes/vehicle/tire_models/brush_tire_model.gd
@@ -37,43 +37,6 @@ func update_tire_forces(slip: Vector2, normal_load: float, surface_mu: float = 1
 		force_vector.x = cornering_stiffness * tan(slip.x) / (1 - slip.y)
 	else:
 		var brushy = (1 - friction * (1 - slip.y) / (4 * deflect)) / deflect
-		force_vector.y = -friction * cornering_stiffness * -slip.y * brushy
+		force_vector.y = friction * cornering_stiffness * slip.y * brushy
 		force_vector.x = friction * cornering_stiffness * tan(slip.x) * brushy
 	return force_vector
-
-
-#func get_tire_forces(slip: Vector2, normal_load: float, mu: float = 1.0) -> Vector3:
-#	var stiffness = 1000000 + 8000000 * (1 - tire_softness)
-#	var cornering_stiffness = 0.5 * stiffness * pow(contact_patch, 2)
-#	var friction = mu * normal_load
-#
-#	var lambda = 0.04
-#
-#	var lat_critical_slip = friction / (2 * cornering_stiffness)
-#	var long_critical_slip = friction / (stiffness * contact_patch * (contact_patch + lambda))
-#	var lat_critical_length = 0
-#	var long_critical_length = 0
-#	if slip.x:
-#		lat_critical_length = friction / (stiffness * contact_patch * tan(abs(slip.x)))
-#
-#	var force_vector := Vector3.ZERO
-	
-	# Self aligning moment
-#	if lat_critical_length >= contact_patch:
-#		force_vector.x = cornering_stiffness * slip.x
-#		force_vector.z = cornering_stiffness * contact_patch * slip.x / 6
-#	else:
-#		if slip.x:
-#			force_vector.x = friction - (pow(friction,2) / (4 * cornering_stiffness * tan(slip.x)))
-#			var idk = (mu * pow(normal_load, 2) / (12 * contact_patch * stiffness * tan(slip.x)))
-#			force_vector.z = idk * (3 - ((2 * friction) / (pow(contact_patch, 2) * stiffness * tan(slip.x))))
-#	print(force_vector.z)
-	
-	# Longitudinal forces
-#	if abs(slip.y) < long_critical_slip:
-#		long_critical_length = contact_patch
-#		force_vector.y = stiffness * contact_patch * (lambda + 0.5 * contact_patch) * slip.y
-#	else:
-#		long_critical_length = friction / (contact_patch * stiffness * slip.y) - lambda
-#		force_vector.y = stiffness * slip.y * long_critical_length * (lambda + 0.5 * long_critical_length) + friction * (1 - long_critical_length / contact_patch)
-#	return force_vector
