4c6485525551f4ded9fe2f00263539fbf2dcdcc7
Veeti Väisänen <veetiv.aisanen1@gmail.com>
2023-04-27 12:59:48 +0300

    Update default values for wheel_params

--- scenes/vehicle/wheel_params.gd
+++ scenes/vehicle/wheel_params.gd
@@ -3,9 +3,9 @@ extends Resource
 
 @export var tire_model: BaseTireModel
 @export var spring_length := 0.15
-@export var spring_stiffness := 45000.0
-@export var bump := 10000.0
-@export var rebound := 11000.0
+@export var spring_stiffness := 45.0
+@export var bump := 6.0
+@export var rebound := 7.0
 @export var wheel_mass := 20.0
 @export var tire_radius := 0.3
 @export var tire_width := 0.225
